37a5a0e97b206e213361985db076e1e30a2a4a8f
Sudhir Shinde <43815012+sudhir5595@users.noreply.github.com>
2020-04-14 18:42:37 +0530

    day 1

---
+++ Single Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // unordered_map <int, int> mp;
+        int res = 0;
+        for(int i = 0; i<nums.size(); i++){
+            // mp[nums[i]]++;
+            res = nums[i]^res;
+        }
+        
+        return res;
+    }
+};
