539779c34d91811d96f5b35dac4699c52d87ebae
Sudhir Shinde <43815012+sudhir5595@users.noreply.github.com>
2020-04-21 09:09:47 +0530

    Create Contiguous Array.cpp

---
+++ Contiguous Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int sum = 0;
+        int res = 0;
+        for(int i = 0; i<nums.size(); i++){
+            if(nums[i] == 0){
+                nums[i] = -1;
+            }
+        }
+        unordered_map <int, int> mp;
+        mp[0] = 0;
+        for(int i = 0; i<nums.size(); i++){
+            sum += nums[i];
+            if(mp.find(sum) != mp.end()){
+                res = max(res, i- mp[sum] + 1);
+            }
+            else{
+                mp[sum] = i + 1;
+            }
+        }
+        return res;
+    }
+};
